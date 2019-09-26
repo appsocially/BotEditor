2615fec74c63be60b52b2f2367a122636297d2ab
Kaoru Hosokawa <kaoru.hosokawa@chatcenter.io>
2019-09-26 10:47:35 +0900

    support goto

--- src/components/exportUPIL.js
+++ src/components/exportUPIL.js
@@ -7,14 +7,37 @@ const MAIN_DIALOG = 'mainDialog'
 const exportScenarioAsUPIL = (scenarioArray) => {
     console.log(JSON.stringify(scenarioArray, null, 4))
 
+    const gotoTargets = getGotoTargets(scenarioArray)
+    console.log(JSON.stringify(gotoTargets, null, 4))
+
+    const entities = createMainDialog(MAIN_DIALOG, scenarioArray)
+    console.log(JSON.stringify(entities, null, 4))
+
+    const dialogs = extractDialogsForGoto(entities[0], gotoTargets)
+    console.log('dialogs', JSON.stringify(dialogs, null, 4))
+
     let upil = []
     upil = upil.concat(createAllDialogs(scenarioArray))
-    upil = upil.concat(createMainDialog(MAIN_DIALOG, scenarioArray))
+    upil = upil.concat(createGotoTargetDialogs(dialogs))
+    upil = upil.concat(createMainDialogString(MAIN_DIALOG, entities))
     upil = upil.concat(createRun(MAIN_RUN, MAIN_DIALOG))
 
     console.log(pretty(upil))
 
     return upil
+}
+
+const getGotoTargets = (scenarioArray) => {
+    const targets = new Set()
+
+    for (const node of scenarioArray) {
+        const { type, toId } = node
+        if (type === 'goto') {
+            targets.add(toId.replace(/-/g, '_'))
+        }
+    }
+
+    return [...targets]
 }
 
 const getStartId = (scenarioArray) => {
@@ -60,17 +83,17 @@ const createAllDialogs = (scenarioArray) => {
     let upil = []
 
     for (let node of scenarioArray) {
-        let { type, id, text, customVariable, selections } = node
+        let { type, id, text, customVariable, selections, toId } = node
         id = id.replace(/-/g, '_')   // Should be fixed by BotEditor
         switch (type) {
             case 'normal':
-                upil = upil.concat(createDialog(id, createTemplate(id, text)))
+                upil = upil.concat(createDialogString(id, createTemplateString(id, text)))
                 break
             case 'openquestion':
-                upil = upil.concat(createDialog(id, createOpenQuestion(id, text, customVariable)))
+                upil = upil.concat(createDialogString(id, createOpenQuestionString(id, text, customVariable)))
                 break
             case 'selection':
-                upil = upil.concat(createDialog(id, createSelection(id, text, customVariable, selections)))
+                upil = upil.concat(createDialogString(id, createSelectionString(id, text, customVariable, selections)))
                 break
             case 'goto':
             case 'start-point':
@@ -88,6 +111,14 @@ const createMainDialog = (name, scenarioArray) => {
     let upil = []
     const startId = getStartId(scenarioArray)
     upil = upil.concat(createDialog(name, createCalls(startId, scenarioArray)))
+    return upil
+}
+
+const createMainDialogString = (name, entities) => {
+    console.log('entities:', JSON.stringify(entities, null, 4))
+    let upil = []
+    const entityStrings = createStatementStrings(entities[0].statements)
+    upil = upil.concat(createDialogString(name, entityStrings))
     return upil
 }
 
@@ -125,11 +156,11 @@ const createCalls = (nodeId, scenarioArray) => {
         return []
     }
 
-    let { id, type, conditions } = node
+    let { id, type, conditions, toId } = node
 
     conditions = cleanseConditions(conditions)
 
-    statements = statements.concat(createCallIfRequired(type, id, scenarioArray))
+    statements = statements.concat(createCallIfRequired(type, id, toId, scenarioArray))
     switch (conditions.length) {
         case 0:
             // Do nothing
@@ -144,23 +175,24 @@ const createCalls = (nodeId, scenarioArray) => {
     return statements
 }
 
-const createCallIfRequired = (type, id, scenarioArray) => {
+const createCallIfRequired = (type, id, toId, scenarioArray) => {
     let statements = []
 
     if (type === 'start-point') {
         // not required
         return []
     }
-    if (type === 'goto') {
-        // ignore - No equivalent construct in UPIL
-        return []
-    }
 
     let correctedId = id.replace(/-/g, '_')   // Should be fixed by BotEditor
-    statements = statements.concat(createCall(correctedId))
 
-    if (type === 'selection') {
-        statements = statements.concat(createIfCallsForSelection(id, scenarioArray))
+    if (type === 'goto') {
+        toId = toId.replace(/-/g, '_')   // Should be fixed by BotEditor
+        statements = statements.concat(createGoto(correctedId, toId))
+    } else {
+        statements = statements.concat(createCall(correctedId))
+        if (type === 'selection') {
+            statements = statements.concat(createIfCallsForSelection(id, scenarioArray))
+        }
     }
 
     return statements
@@ -193,13 +225,49 @@ const createIfCallsForSelection = (id, scenarioArray) => {
     return statements.concat(createIfCalls(ifCallConditions, scenarioArray))
 }
 
+const createGoto = (name, toId) => {
+    let statement = []
+    statement.push({
+        type: 'goto',
+        name,
+        toId
+    })
+    return statement
+}
+
+const createGotoString = (name, toId) => {
+    let statement = []
+    toId = toId.replace(/-/g, '_')   // Should be fixed by BotEditor
+    statement.push(`...goto_target_${toId}`)
+    return statement
+}
+
 const createCall = (name) => {
+    let statement = []
+    statement.push({
+        type: 'embed',
+        name
+    })
+    return statement
+}
+
+const createCallString = (name) => {
     let statement = []
     statement.push(`...${name}`)
     return statement
 }
 
 const createDialog = (name, statements) => {
+    let statement = []
+    statement.push({
+        type: 'dialog',
+        name,
+        statements
+    })
+    return statement
+}
+
+const createDialogString = (name, statements) => {
     let statement = []
     statement.push(`DIALOG ${name}`)
     statement = statement.concat(statements)
@@ -229,29 +297,70 @@ const createIfCalls = (conditions, scenarioArray) => {
 
 const createIf = (condition, statements) => {
     let statement = []
+
     const { customVarName: left, operator, comparedValue: right } = condition
-    statement.push(`IF ${left} ${operator} "${right}"`)
-    statement.push('DIALOG')
-    statement = statement.concat(statements)
-    statement.push('/DIALOG')
+    statement.push({
+        type: 'if',
+        left,
+        operator,
+        right,
+        statements
+    })
+
     return statement
 }
 
 const createElif = (condition, statements) => {
     let statement = []
+
     const { customVarName: left, operator, comparedValue: right } = condition
-    statement.push(`ELIF ${left} ${operator} "${right}"`)
-    statement.push('DIALOG')
-    statement = statement.concat(statements)
-    statement.push('/DIALOG')
+    statement.push({
+        type: 'elif',
+        left,
+        operator,
+        right,
+        statements
+    })
+
     return statement
 }
 
 const createElse = (statements) => {
     let statement = []
+
+    statement.push({
+        type: 'else',
+        statements
+    })
+
+    return statement
+}
+
+const createIfString = (condition, statements) => {
+    let statement = []
+    const { customVarName: left, operator, comparedValue: right } = condition
+    statement.push(`IF ${left} ${operator} "${right}"`)
+    statement.push('DIALOG')
+    statement = statement.concat(createStatementStrings(statements))
+    statement.push('/DIALOG')
+    return statement
+}
+
+const createElifString = (condition, statements) => {
+    let statement = []
+    const { customVarName: left, operator, comparedValue: right } = condition
+    statement.push(`ELIF ${left} ${operator} "${right}"`)
+    statement.push('DIALOG')
+    statement = statement.concat(createStatementStrings(statements))
+    statement.push('/DIALOG')
+    return statement
+}
+
+const createElseString = (statements) => {
+    let statement = []
     statement.push('ELSE')
     statement.push('DIALOG')
-    statement = statement.concat(statements)
+    statement = statement.concat(createStatementStrings(statements))
     statement.push('/DIALOG')
     statement.push('/IF')
     return statement
@@ -265,7 +374,7 @@ const createRun = (name, body) => {
     return statement
 }
 
-const createTemplate = (name, body) => {
+const createTemplateString = (name, body) => {
     let statement = []
     statement.push(`TEMPLATE ${name}`)
     statement.push(`"${body}"`)
@@ -273,7 +382,7 @@ const createTemplate = (name, body) => {
     return statement
 }
 
-const createOpenQuestion = (name, body, variable) => {
+const createOpenQuestionString = (name, body, variable) => {
     const { location, varType } = variable
     let statement = []
     statement.push(`TEMPLATE ${name}`)
@@ -283,7 +392,7 @@ const createOpenQuestion = (name, body, variable) => {
     return statement
 }
 
-const createSelection = (name, body, variable, selections) => {
+const createSelectionString = (name, body, variable, selections) => {
     const { location, varType } = variable
     let statement = []
     statement.push(`SELECT ${name}`)
@@ -295,6 +404,85 @@ const createSelection = (name, body, variable, selections) => {
     statement.push(`>>${location}:${varType.toLowerCase()}`)
     statement.push('/SELECT')
     return statement
+}
+
+const createStatementStrings = (statements) => {
+    let statementStrings = []
+
+    for (const statement of statements) {
+        const { type, name, toId, left, operator, right, statements: stats } = statement
+        const condition = {
+            customVarName: left,
+            operator,
+            comparedValue: right
+        }
+        switch (type) {
+            case 'embed':
+                statementStrings = statementStrings.concat(createCallString(name))
+                break
+            case 'goto':
+                statementStrings = statementStrings.concat(createGotoString(name, toId))
+                break
+            case 'if':
+                statementStrings = statementStrings.concat(createIfString(condition, stats))
+                break
+            case 'elif':
+                statementStrings = statementStrings.concat(createElifString(condition, stats))
+                break
+            case 'else':
+                statementStrings = statementStrings.concat(createElseString(stats))
+                break
+            default:
+                console.log(`Unknown entity type: ${type}`)
+        }
+    }
+
+    return statementStrings
+}
+
+const createGotoTargetDialogs = (dialogs) => {
+    let entityStrings = []
+
+    for (const dialog of dialogs) {
+        const { name, statements } = dialog
+        statements = createStatementStrings(statements)
+        entityStrings = entityStrings.concat(createDialogString(name, statements))
+    }
+
+    return entityStrings
+}
+
+const extractADialog = (statements, target) => {
+    let dialog = {}
+    let inDialog = false
+    for (const statement of statements) {
+        if (inDialog) {
+            dialog.statements.push(statement)
+        } else {
+            const { type, name, statements: stats } = statement
+            if (type === 'embed' && name === target) {
+                inDialog = true
+                dialog = {
+                    type: 'dialog',
+                    name: `goto_target_${name}`,
+                    statements: [statement]
+                }
+            } else if (['if', 'elif', 'else'].includes(type)) {
+                const aDialog = extractADialog(stats, target)
+                if (Object.keys(aDialog).length > 0) {
+                    return aDialog
+                }
+            }
+        }
+    }
+    return dialog
+}
+
+const extractDialogsForGoto = (entity, gotoTargets) => {
+    const dialogs = gotoTargets.map(target => {
+        return extractADialog(entity.statements, target)
+    })
+    return dialogs
 }
 
 const pretty = (statements) => {

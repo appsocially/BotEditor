ac3c593c0bdd13606bf78c0e487d3bed5fb32226
Kaoru Hosokawa <kaoru.hosokawa@chatcenter.io>
2019-09-26 22:07:24 +0900

    update lint to check for selection without next step

--- src/components/lintScenario.js
+++ src/components/lintScenario.js
@@ -1,158 +1,232 @@
-const lintScenario = {}
-export default lintScenario
+const lintScenario = {};
+export default lintScenario;
 
-const lintScenarioForUPILOutput = (scenario) => {
-    let warnings = []
-    let errors = []
+const lintScenarioForUPILOutput = scenario => {
+  let warnings = [];
+  let errors = [];
 
-    scenario = scenario.sort((thisElem, thatElem) => {
-        const { num: a } = thisElem
-        const { num: b } = thatElem
-        if (a < b) { return -1 }
-        else if (a === b) { return 0 }
-        else { return 1 }
-    })
-
-    for (const node of scenario) {
-        console.log(JSON.stringify(node, null, 2))
-
-        let result = {}
-        const { num, type } = node
-        switch (type) {
-            case 'start-point':
-                result = lintStartPoint(node, scenario)
-                break
-            case 'normal':
-                result = lintNormal(node, scenario)
-                break
-            case 'openquestion':
-                result = lintOpenQuestion(node, scenario)
-                break
-            case 'selection':
-                result = lintSelection(node, scenario)
-                break
-            case 'goto':
-                result = lintGoTo(node, scenario)
-                break
-            case 'media':
-                result = lintMedia(node, scenario)
-                break
-            default:
-                result = { warnings: [], errors: [error(num, `Unknown type: ${type}`)] }
-        }
-        warnings = warnings.concat(result.warnings)
-        errors = errors.concat(result.errors)
+  scenario = scenario.sort((thisElem, thatElem) => {
+    const { num: a } = thisElem;
+    const { num: b } = thatElem;
+    if (a < b) {
+      return -1;
+    } else if (a === b) {
+      return 0;
+    } else {
+      return 1;
     }
+  });
 
-    const result = errors.length === 0 ? { result: 'success', warnings } : { result: 'fail', warnings, errors }
+  for (const node of scenario) {
+    console.log(JSON.stringify(node, null, 2));
 
-    console.log(JSON.stringify(result, null, 2))
+    let result = {};
+    const { num, type } = node;
+    switch (type) {
+      case "start-point":
+        result = lintStartPoint(node, scenario);
+        break;
+      case "normal":
+        result = lintNormal(node, scenario);
+        break;
+      case "openquestion":
+        result = lintOpenQuestion(node, scenario);
+        break;
+      case "selection":
+        result = lintSelection(node, scenario);
+        break;
+      case "goto":
+        result = lintGoTo(node, scenario);
+        break;
+      case "media":
+        result = lintMedia(node, scenario);
+        break;
+      default:
+        result = {
+          warnings: [],
+          errors: [error(num, `Unknown type: ${type}`)]
+        };
+    }
+    warnings = warnings.concat(result.warnings);
+    errors = errors.concat(result.errors);
+  }
 
-    return result
-}
+  const result =
+    errors.length === 0
+      ? { result: "success", warnings }
+      : { result: "fail", warnings, errors };
+
+  console.log(JSON.stringify(result, null, 2));
+
+  return result;
+};
 
 const lintStartPoint = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    let { num, conditions } = node
-    conditions = conditions === undefined ? [] : conditions
-    for (const condition of conditions) {
-        const { type, option, next } = condition
-        if (type === 'custom_var' && option === undefined) {
-            const toNode = getNodeFromId(next, scenario)
-            result.errors.push(error(num, `UPIL export needs edge Start -> ${toNode.num} to have a custom variable. Add one.`))
-        } else if (type === 'default') {
-            const toNode = getNodeFromId(next, scenario)
-            result.errors.push(error(num, `UPIL export needs edge Start -> ${toNode.num} to have a custom variable. Add one.`))
-        }
+  let { num, conditions } = node;
+  conditions = conditions === undefined ? [] : conditions;
+  for (const condition of conditions) {
+    const { type, option, next } = condition;
+    if (type === "custom_var" && option === undefined) {
+      const toNode = getNodeFromId(next, scenario);
+      result.errors.push(
+        error(
+          num,
+          `UPIL export needs edge Start -> ${
+            toNode.num
+          } to have a custom variable. Add one.`
+        )
+      );
+    } else if (type === "default") {
+      const toNode = getNodeFromId(next, scenario);
+      result.errors.push(
+        error(
+          num,
+          `UPIL export needs edge Start -> ${
+            toNode.num
+          } to have a custom variable. Add one.`
+        )
+      );
     }
+  }
 
-    return result
-}
+  return result;
+};
 
 const lintNormal = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    let { num, customAction, conditions } = node
-    conditions = conditions === undefined ? [] : conditions
-    for (const condition of conditions) {
-        const { type, option, next } = condition
-        if (type === 'custom_var' && option === undefined) {
-            const toNode = getNodeFromId(next, scenario)
-            result.errors.push(error(num, `UPIL export needs edge ${num} -> ${toNode.num} to have a custom variable. Add one.`))
-        } else if (type === 'default') {
-            const toNode = getNodeFromId(next, scenario)
-            result.errors.push(error(num, `UPIL export needs edge ${num} -> ${toNode.num} to have a custom variable. Add one.`))
-        }
+  let { num, customAction, conditions } = node;
+  conditions = conditions === undefined ? [] : conditions;
+  for (const condition of conditions) {
+    const { type, option, next } = condition;
+    if (type === "custom_var" && option === undefined) {
+      const toNode = getNodeFromId(next, scenario);
+      result.errors.push(
+        error(
+          num,
+          `UPIL export needs edge ${num} -> ${
+            toNode.num
+          } to have a custom variable. Add one.`
+        )
+      );
+    } else if (type === "default") {
+      const toNode = getNodeFromId(next, scenario);
+      result.errors.push(
+        error(
+          num,
+          `UPIL export needs edge ${num} -> ${
+            toNode.num
+          } to have a custom variable. Add one.`
+        )
+      );
     }
+  }
 
-    if (customAction !== undefined) {
-        result.warnings.push(warning(num, `UPIL export does not support Custom Action. Ignored.`))
-    }
+  if (customAction !== undefined) {
+    result.warnings.push(
+      warning(num, `UPIL export does not support Custom Action. Ignored.`)
+    );
+  }
 
-    return result
-}
+  return result;
+};
 
 const lintOpenQuestion = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    const { num, customVariable, customAction } = node
-    if (customVariable === undefined) {
-        result.errors.push(error(num, `UPIL export needs Open Question to have a custom variable. Add one.`))
-    } else if (customVariable.location === '') {
-        result.errors.push(error(num, `Open Question's custom variable is missing a location. Add one.`))
-    }
-    if (customAction !== undefined) {
-        result.warnings.push(warning(node.num, `UPIL export does not support Custom Action. Ignored.`))
-    }
+  const { num, customVariable, customAction } = node;
+  if (customVariable === undefined) {
+    result.errors.push(
+      error(
+        num,
+        `UPIL export needs Open Question to have a custom variable. Add one.`
+      )
+    );
+  } else if (customVariable.location === "") {
+    result.errors.push(
+      error(
+        num,
+        `Open Question's custom variable is missing a location. Add one.`
+      )
+    );
+  }
+  if (customAction !== undefined) {
+    result.warnings.push(
+      warning(node.num, `UPIL export does not support Custom Action. Ignored.`)
+    );
+  }
 
-    return result
-}
+  return result;
+};
 
 const lintSelection = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    const { num, customVariable, customAction } = node
-    if (customVariable === undefined) {
-        result.errors.push(error(num, `UPIL export needs Selection to have a custom variable. Add one.`))
-    } else if (customVariable.location === '') {
-        result.errors.push(error(num, `Selection's custom variable is missing a location. Add one.`))
-    }
-    if (customAction !== undefined) {
-        result.warnings.push(warning(node.num, `UPIL export does not support Custom Action. Ignored.`))
-    }
+  const { num, selections, customVariable, customAction } = node;
 
-    return result
-}
+  for (const selection of selections) {
+    const { label, conditions } = selection;
+    if (conditions === undefined) {
+      result.errors.push(
+        error(
+          num,
+          `'${label}' selection does not have the next step. Export UPIL needs all selection to have a next step.`
+        )
+      );
+    }
+  }
+
+  if (customVariable === undefined) {
+    result.errors.push(
+      error(
+        num,
+        `UPIL export needs Selection to have a custom variable. Add one.`
+      )
+    );
+  } else if (customVariable.location === "") {
+    result.errors.push(
+      error(num, `Selection's custom variable is missing a location. Add one.`)
+    );
+  }
+  if (customAction !== undefined) {
+    result.warnings.push(
+      warning(node.num, `UPIL export does not support Custom Action. Ignored.`)
+    );
+  }
+
+  return result;
+};
 
 const lintGoTo = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    // Nothing to check
+  // Nothing to check
 
-    return result
-}
+  return result;
+};
 
 const lintMedia = (node, scenario) => {
-    let result = { warnings: [], errors: [] }
+  let result = { warnings: [], errors: [] };
 
-    // Nothing to check
+  // Nothing to check
 
-    return result
-}
+  return result;
+};
 
 const warning = (num, mess) => {
-    return `[Node ${num}] ${mess}`
-}
+  return `[Node ${num}] ${mess}`;
+};
 
 const error = (num, mess) => {
-    return `[Node ${num}] ${mess}`
-}
+  return `[Node ${num}] ${mess}`;
+};
 
 const getNodeFromId = (id, scenario) => {
-    return scenario.find(node => {
-        return id === node.id
-    })
-}
+  return scenario.find(node => {
+    return id === node.id;
+  });
+};
 
-window.lintScenarioForUPILOutput = lintScenarioForUPILOutput
+window.lintScenarioForUPILOutput = lintScenarioForUPILOutput;
